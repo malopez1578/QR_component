58b67869a5da74f7ccb6a0a3b276373ffef3f530
miguello <miguello@titamedia.com>
2023-08-15 18:18:43 -0500

    test(qr-component): ✨ se realiza primer test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 // Imports
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { beforeAll, describe, expect, it } from 'vitest';
 
 // To Test
@@ -8,45 +8,15 @@ import App from './App';
 describe('Renders main page correctly', async () => {
     beforeAll(async () => {
         // Setup
-        await render(<App />);
+        render(<App />);
     });
     /**
      * Passes - shows title correctly
      */
     it('Should render the page correctly', async () => {
-        const h1 = await screen.queryByText('Vite + React');
+        const h1 = screen.queryByText('Improve your front-end skills by building projects');
 
         // Post Expectations
         expect(h1).not.toBeNull();
-    });
-
-    /**
-     * Passes - shows the button count correctly present
-     */
-    it('Should show the button count set to 0', async () => {
-        // Setup
-        const button = await screen.queryByText('count is 0');
-
-        // Expectations
-        expect(button).not.toBeNull();
-    });
-
-    /**
-     * Passes - clicks the button 3 times and shows the correct count
-     */
-    it('Should show the button count set to 3', async () => {
-        // Setup
-        const button = await screen.queryByTestId('count');
-        
-        // Pre Expectations
-        expect(button).not.toBeNull();
-
-        // Actions
-        fireEvent.click(button as HTMLElement);
-        fireEvent.click(button as HTMLElement);
-        fireEvent.click(button as HTMLElement);
-        
-        // Post Expectations
-        expect(button?.innerHTML).toBe('count is 3');
     });
 });
